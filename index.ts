cf627b67a4abf5a5a7b4d406ac7708474bf70a4c
omar2205 <omar2205a@yahoo.com>
2022-11-11 08:36:37 +0000

    first commit

---
+++ index.ts
@@ -0,0 +1,37 @@
+import express from 'express'
+import helmet from 'helmet'
+import cors from 'cors'
+import morgan from 'morgan'
+import rfs from 'rotating-file-stream'
+import path, { dirname } from 'path'
+
+import { fileURLToPath } from 'url'
+
+// routes
+import v1_router from './routes/v1/index.js'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const app = express()
+const PORT = process.env.PORT || 3000
+// create a rotating write stream
+const accessLogStream = rfs.createStream('access.log', {
+  interval: '1d', // rotate daily
+  path: path.join(__dirname, 'log'),
+})
+
+const logger = morgan('combined', { stream: accessLogStream })
+
+app.use(helmet())
+app.use(cors())
+app.use(logger)
+
+app.use('/api/v1', v1_router)
+
+app.get('/', (req, res) => {
+  res.send()
+})
+
+app.listen(PORT, () => {
+  console.log('⚡ Server online - port:', PORT)
+})
